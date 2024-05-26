7711442009d7b9fdb3744b3298ad04bf3c85d87a
Baali Boudjemaa <baaliboudjemaaens@gmail.com>
2024-05-26 10:15:18 +0100

    Update cosmos_epub.dart

--- lib/cosmos_epub.dart
+++ lib/cosmos_epub.dart
@@ -12,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart' as http;
 
 class CosmosEpub {
   static final GlobalKey<NavigatorState> navigatorKey =
